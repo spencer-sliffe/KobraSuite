a01ffa1d23967dfa6ada30a76cb9c207a69ea456
Spencer Sliffe <spenceronw@ku.edu>
2024-12-20 16:38:28 -0600

    Update settings.py
    needed a push

--- kobrasuitecore/kobrasuitecore/settings.py
+++ kobrasuitecore/kobrasuitecore/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
