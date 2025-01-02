67ce5eaa70a75f982b944fc85d83a9f1265a1645
Spencer Sliffe <spenceronw@ku.edu>
2025-01-02 13:47:36 -0600

    Update auth_service.dart

--- kobrasuite_app/lib/services/auth_service.dart
+++ kobrasuite_app/lib/services/auth_service.dart
@@ -3,10 +3,7 @@ import 'package:http/http.dart' as http;
 
 class AuthService {
   // Replace with your actual backend base URL
-  static const String baseUrl = String.fromEnvironment(
-    'BASE_URL',
-    defaultValue: 'http://127.0.0.1:8000',
-  );
+  static const String baseUrl = 'https://kobrasuite-backend.azurewebsites.net';
 
   // A place to store the session cookie in memory
   static String? sessionCookie;
